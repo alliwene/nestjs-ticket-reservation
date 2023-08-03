f65fcd8061fc7f36cc2e53a2a522ee82339fe26a
Abiodun Allison <allisonabiodun@gmail.com>
2023-08-03 16:21:50 +0100

    feat: start building stripe payments app

---
+++ apps/payments/src/payments.controller.ts
@@ -0,0 +1,14 @@
+import { Controller } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { CreateChargeDto } from './dto/create-charge.dto';
+
+@Controller()
+export class PaymentsController {
+  constructor(private readonly paymentsService: PaymentsService) {}
+
+  @MessagePattern('create_charge')
+  async createCharge(@Payload() { card, amount }: CreateChargeDto) {
+    return this.paymentsService.createCharge({ card, amount });
+  }
+}
---
+++ apps/payments/src/payments.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from '@app/common';
+
+import { PaymentsController } from './payments.controller';
+import { PaymentsService } from './payments.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: './apps/payments/.env',
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+        STRIPE_SECRET_KEY: Joi.string().required(),
+      }),
+    }),
+    LoggerModule,
+  ],
+  controllers: [PaymentsController],
+  providers: [PaymentsService],
+})
+export class PaymentsModule {}
