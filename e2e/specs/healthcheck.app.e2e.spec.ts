a1bb72c5e4090825f365569ea330367ef28c38b1
Abiodun Allison <allisonabiodun@gmail.com>
2023-10-23 16:12:29 +0100

    feat: e2e healthchecks for payments and notifications using tcp-ping

--- e2e/specs/healthcheck.app.e2e.spec.ts
+++ e2e/specs/healthcheck.app.e2e.spec.ts
@@ -1,3 +1,5 @@
+import { ping } from 'tcp-ping';
+
 describe('Health', () => {
   test('Reservations', async () => {
     const response = await fetch('http://reservations:3000');
@@ -7,5 +9,19 @@ describe('Health', () => {
   test('Auth', async () => {
     const response = await fetch('http://auth:3001');
     expect(response.ok).toBeTruthy();
+  });
+
+  test('Payments', (done) => {
+    ping({ address: 'payments', port: 3003 }, (err) => {
+      if (err) fail();
+      done();
+    });
+  });
+
+  test('Notifications', (done) => {
+    ping({ address: 'notifications', port: 3004 }, (err) => {
+      if (err) fail();
+      done();
+    });
   });
 });
