75d6646681a77899d496fb5e77f1191924c2ecb3
Abiodun Allison <allisonabiodun@gmail.com>
2023-10-25 23:02:34 +0100

    feat: guard ticket deletion with admin user role

--- apps/reservations/src/reservations.controller.ts
+++ apps/reservations/src/reservations.controller.ts
@@ -12,7 +12,7 @@ import {
 import { ReservationsService } from './reservations.service';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 import { UpdateReservationDto } from './dto/update-reservation.dto';
-import { CurrentUser, JwtAuthGuard, UserDto } from '@app/common';
+import { CurrentUser, JwtAuthGuard, Roles, UserDto } from '@app/common';
 
 @Controller('reservations')
 @UseGuards(JwtAuthGuard)
@@ -53,6 +53,7 @@ export class ReservationsController {
     return this.reservationsService.update(id, updateReservationDto);
   }
 
+  @Roles('Admin')
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.reservationsService.remove(id);
