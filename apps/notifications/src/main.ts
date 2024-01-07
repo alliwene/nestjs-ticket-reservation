d0d8a29b746f8047f3135ad84aeeada1ad0ed6dd
Abiodun Allison <allisonabiodun@gmail.com>
2024-01-07 08:09:39 +0100

    feat: refactor notifications to use grpc

--- apps/notifications/src/main.ts
+++ apps/notifications/src/main.ts
@@ -2,18 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from 'nestjs-pino';
+import { join } from 'path';
 
 import { NotificationsModule } from './notifications.module';
+import { NOTIFICATIONS_PACKAGE_NAME } from '@app/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(NotificationsModule);
 
   const configService = app.get(ConfigService);
   app.connectMicroservice({
-    transport: Transport.TCP,
+    transport: Transport.GRPC,
     options: {
-      host: '0.0.0.0',
-      port: configService.get('PORT'),
+      package: NOTIFICATIONS_PACKAGE_NAME,
+      protoPath: join(__dirname, '../../../proto/notifications.proto'),
+      url: configService.getOrThrow('NOTIFICATIONS_GRPC_URL'),
     },
   });
 
--- apps/notifications/src/notifications.controller.ts
+++ apps/notifications/src/notifications.controller.ts
@@ -1,16 +1,19 @@
 import { Controller, UsePipes, ValidationPipe } from '@nestjs/common';
-import { EventPattern, Payload } from '@nestjs/microservices';
 
 import { NotificationsService } from './notifications.service';
 import { NotifyEmailDto } from './dto/notify-email.dto';
+import {
+  NotificationsServiceController,
+  NotificationsServiceControllerMethods,
+} from '@app/common';
 
 @Controller()
-export class NotificationsController {
+@NotificationsServiceControllerMethods()
+export class NotificationsController implements NotificationsServiceController {
   constructor(private readonly notificationsService: NotificationsService) {}
 
   @UsePipes(new ValidationPipe())
-  @EventPattern('notify_email')
-  async notifyEmail(@Payload() { email, text }: NotifyEmailDto) {
+  async notifyEmail({ email, text }: NotifyEmailDto) {
     this.notificationsService.notifyEmail({ email, text });
   }
 }
