cd4e456bd26f8fc216ef4370a4af4e95095d7bae
Abiodun Allison <allisonabiodun@gmail.com>
2024-01-08 10:14:42 +0100

    feat: add graphql apollo server config

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { GatewayController } from './gateway.controller';
-import { GatewayService } from './gateway.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { IntrospectAndCompose } from '@apollo/gateway';
+
+import { LoggerModule } from '@app/common';
 
 @Module({
-  imports: [],
-  controllers: [GatewayController],
-  providers: [GatewayService],
+  imports: [
+    GraphQLModule.forRootAsync<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      useFactory: (configService: ConfigService) => ({
+        gateway: {
+          supergraphSdl: new IntrospectAndCompose({
+            subgraphs: [],
+          }),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: './apps/gateway/.env',
+    }),
+    LoggerModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class GatewayModule {}
--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
+
 import { GatewayModule } from './gateway.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
-  await app.listen(3000);
+  app.useLogger(app.get(Logger));
+
+  const configService = app.get(ConfigService);
+  await app.listen(configService.getOrThrow('PORT'));
 }
 bootstrap();
