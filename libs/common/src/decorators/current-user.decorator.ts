3b29c6079aae6dcc60d4221f9d925f3ddcfb0d21
Abiodun Allison <allisonabiodun@gmail.com>
2024-01-14 10:37:26 +0100

    chore: fix bug in CurrentUser

--- libs/common/src/decorators/current-user.decorator.ts
+++ libs/common/src/decorators/current-user.decorator.ts
@@ -1,21 +1,15 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 
-import { UserDocument } from '../models/user.schema';
-
-const getCurrentUserByContext = (context: ExecutionContext): UserDocument => {
-  if (context.getType() === 'http') {
-    const request = context.switchToHttp().getRequest();
-    return request.user;
-  }
-
-  const user = context.getArgs()[2]?.req.headers?.user;
-  if (user) {
-    return JSON.parse(user);
-  }
-};
-
 export const CurrentUser = createParamDecorator(
   (_data: unknown, context: ExecutionContext) => {
-    getCurrentUserByContext(context);
+    if (context.getType() === 'http') {
+      const request = context.switchToHttp().getRequest();
+      return request.user;
+    }
+
+    const user = context.getArgs()[2]?.req.headers?.user;
+    if (user) {
+      return JSON.parse(user);
+    }
   },
 );
