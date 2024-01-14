f202c3454d433e0b2979097da78a8b18fafb571a
Abiodun Allison <allisonabiodun@gmail.com>
2024-01-14 09:25:06 +0100

    feat: add graphql support to CurrentUser decorator

--- libs/common/src/decorators/current-user.decorator.ts
+++ libs/common/src/decorators/current-user.decorator.ts
@@ -1,9 +1,20 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 import { UserDto } from '../dto';
 
-export const CurrentUser = createParamDecorator(
-  (_data: unknown, context: ExecutionContext) => {
+const getCurrentUserByContext = (context: ExecutionContext) => {
+  if (context.getType() === 'http') {
     const request = context.switchToHttp().getRequest();
     return request.user as UserDto;
+  }
+
+  const user = context.getArgs()[2]?.req.headers?.user;
+  if (user) {
+    return JSON.parse(user);
+  }
+};
+
+export const CurrentUser = createParamDecorator(
+  (_data: unknown, context: ExecutionContext) => {
+    getCurrentUserByContext(context);
   },
 );
